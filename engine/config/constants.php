5d317f5b4b2434976ebac32a8e1a82853a95a707
fandis <great_beluluk@yahoo.com>
2021-10-12 21:31:00 +0700

    Refactor engine/config/constants.php

--- engine/config/constants.php
+++ engine/config/constants.php
@@ -3,5 +3,8 @@ define("DIR", dirname(dirname(__DIR__)));
 define("DS", DIRECTORY_SEPARATOR);
 define("WEBHOME",dirname($_SERVER['SCRIPT_NAME']).'/');
 
-$_config = json_decode(file_get_contents(DIR."/engine/config.json"));
-foreach ($_config->constants as $k=>$v) define($k, $v);
+loadConstants();
+function loadConstants() {
+  $_config = json_decode(file_get_contents(DIR."/engine/config.json"));
+  foreach ($_config->constants as $k=>$v) define($k, $v);  
+}
