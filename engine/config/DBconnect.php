17cf8158d2c35722a5bb1df6e1962edc038362a4
fandis <great_beluluk@yahoo.com>
2021-10-16 01:22:10 +0700

    DBConnect by default load the first database connection.

--- engine/config/DBconnect.php
+++ engine/config/DBconnect.php
@@ -5,3 +5,15 @@ function DBConnect($connectionName) {
   if (!isset($config->DBConnections->$connectionName)) throw new \Exception('DB Connection not found. Please check engine/config.json');
   DB::setConnectionWithObject($config->DBConnections->$connectionName);
 }
+
+$config = json_decode(file_get_contents(DIR."/engine/config.json"));
+$arrDb = (array) $config->DBConnections;
+@$dbConfig = array_shift($arrDb);
+if ($dbConfig === null) { echo 'Failed to get database connection config'; die; }
+else {
+  DB::setConnectionWithObject($dbConfig);
+  try { DB::init(); } catch (\Exception $ex) {
+    echo 'Failed to connect to database<br />';
+    echo $ex->getMessage(); die;
+  }
+}
