c15c62925abd022e748ce1db9527fd511468202b
fandis <great_beluluk@yahoo.com>
2021-10-15 10:51:52 +0700

    Dummy PHP file for constants defined in config json, so php intelephense can detect the constants.

---
+++ dummy.php
@@ -0,0 +1,7 @@
+<?php
+//The purpose of this file is for constants defined in config.json is detected by intelephense or such.
+define('APPNAME', '');
+define('APPNAMESPACE', '');
+define('DOMAIN','');
+define('DEBUG', '');
+define('LOGO_IMAGE','');
