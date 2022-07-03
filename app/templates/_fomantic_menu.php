af383fd7ddca5d7ec98ca368c8bde3d74034c5a8
fandi.susanto.bts <fandi.susanto.bts@gmail.com>
2022-07-03 22:07:53 +0700

    Add black color to fomantic menu

--- app/templates/_fomantic_menu.php
+++ app/templates/_fomantic_menu.php
@@ -2,7 +2,7 @@
 $GLOBALS['colorCodes'] = (object) [
   'red'=>'#db2828', 'orange'=>'#F2711C','yellow'=>'#FBBD08', 'olive'=>'#AAC00F', 'green'=>'#21BA45',
   'teal'=>'#00B5AD', 'blue'=>'#2185D0', 'violet'=>'#6435C9', 'purple'=>'#A333C8', 'pink'=>'#E03997',
-  'brown'=>'#A5673F', 'grey'=>'#767676'
+  'brown'=>'#A5673F', 'grey'=>'#767676', 'black'=>'#000',
 ];
 
 function renderFomanticMenuItem($menuItem, $level=0) {
