50bf11f69202755ed5c538e14e7c8b2dabe5c720
fandi.susanto.bts <fandi.susanto.bts@gmail.com>
2022-10-30 19:53:03 +0700

    Update fomantic UI to 2.9

--- app/templates/standard_js_css.php
+++ app/templates/standard_js_css.php
@@ -2,7 +2,10 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=3.0" />
 <link rel="stylesheet" href="<?=WEBHOME?>css/common.css"/>
 <!-- jquery -->
-<script src="<?= WEBHOME ?>libs/jquery3.3.1.min.js" charset="utf-8"></script>
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.6.1/dist/jquery.min.js"></script>
+<!-- Fomantic UI -->
+<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/fomantic-ui@2.9.0/dist/semantic.min.css">
+<script src="https://cdn.jsdelivr.net/npm/fomantic-ui@2.9.0/dist/semantic.min.js"></script>
 
 <!-- vuejs -->
 <script src="https://unpkg.com/vue@3"></script>
@@ -10,9 +13,6 @@
 <script src="<?= WEBHOME ?>libs/moment.min.js"></script>
 <!-- Csshake -->
 <link rel="stylesheet" href="<?= WEBHOME ?>libs/csshake.min.css" />
-<!-- Fomantic UI -->
-<link rel="stylesheet" href="<?= WEBHOME ?>libs/fomantic-2.8/semantic.min.css" />
-<script src="<?= WEBHOME ?>libs/fomantic-2.8/semantic.min.js" charset="utf-8"></script>
 <!-- Lodash -->
 <script src="<?= WEBHOME ?>libs/lodash.min.js"></script>
 <script type="text/javascript" src="<?= WEBHOME ?>libs/fandi/myAsyncLoading.js"></script>
